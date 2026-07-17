101258add68a0eedf975d2735b8f43bbc415bea7
Nuria Bravo <bravortiz04@gmail.com>
2026-07-17 16:08:28 +0200

    chore: initial spring boot project scaffolding

---
+++ src/main/java/com/mercadona/nuriabravo/NuriabravoApplication.java
@@ -0,0 +1,13 @@
+package com.mercadona.nuriabravo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuriabravoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuriabravoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercadona/nuriabravo/NuriabravoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadona.nuriabravo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuriabravoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
